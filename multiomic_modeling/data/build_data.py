9015850f4422efce00d8a886e34163f69d6273bb
dizam92 <mazido@yahoo.fr>
2021-05-17 11:14:56 -0400

    modification construction dataset k= 2K, 5K, 10K

--- multiomic_modeling/data/build_data.py
+++ multiomic_modeling/data/build_data.py
@@ -5,6 +5,7 @@ import pickle
 import h5py
 from collections import defaultdict
 from scipy.stats import median_abs_deviation
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
 local_file_path_origin='/Volumes/Second Part/TCGA Pan-Cancer (PANCAN)/'
 graham_file_path_origin='/home/maoss2/project/maoss2/tcga_pan_cancer_dataset'
 LOCAL = False
@@ -49,7 +50,7 @@ def select_features_based_on_mad(x, axe=0, nb_features=5000):
     indices_features = np.argsort(mad_all_features)[::-1]
     return indices_features[:nb_features]
 
-def build_file_with_dimentionality_reduction(fichier_path, saving_file_name):
+def build_file_with_dimentionality_reduction(fichier_path, saving_file_name, nb_features_selected=2000):
     hf = h5py.File(f'{saving_file_name}', 'w')
     data = pd.read_csv(fichier_path, sep='\t')
     if 'Sample' in data.columns.values:
@@ -65,9 +66,18 @@ def build_file_with_dimentionality_reduction(fichier_path, saving_file_name):
     patients_names = data.columns.values
     features_names = data.index.values
     data = data.values
-    indices_mad_selected = select_features_based_on_mad(x=data, axe=1, nb_features=2000)
-    data = data[indices_mad_selected]
-    features_names = features_names[indices_mad_selected]
+    if fichier_path.find('cnv') != -1:
+        y = np.ones(data.shape[0])
+        learner = SelectKBest(mutual_info_classif, k=nb_features_selected).fit(X=data, y=y)
+        indices_selected = learner.get_support(indices=True)
+        data = learner.transform(data)
+        features_names = features_names[indices_selected]
+        del y, learner, indices_selected
+    else:
+        indices_mad_selected = select_features_based_on_mad(x=data, axe=1, nb_features=nb_features_selected)
+        data = data[indices_mad_selected]
+        features_names = features_names[indices_mad_selected]
+        del indices_mad_selected
     features_names = [str(x).encode('utf-8') for x in features_names]
     patients_names = [str(x).encode('utf-8') for x in patients_names]
     hf.create_dataset(f'dataset', data=data.T)
@@ -98,8 +108,22 @@ else:
 if __name__ == '__main__':
     # TODO: Si la liste fichiers_path change, il faut update la liste saving_files_names car chaque nom a été écrit pour le fichier correspondant
     fichiers_path = [exon_path, cnv_path, methyl_27_path, methyl_450_path, rna_path, rna_isoforms_path, mirna_path, protein_path]
-    saving_files_names = ['exon_pancan_tcga.h5', 'cnv_pancan_tcga.h5', 'methyl_27_pancan_tcga.h5', 'methyl_450_pancan_tcga.h5', 'rna_pancan_tcga.h5', 'rna_isoforms_pancan_tcga.h5', 'mirna_pancan_tcga.h5', 'protein_pancan_tcga.h5']
-    saving_files_names_reduced = ['exon_pancan_tcga_reduced.h5', 'cnv_pancan_tcga_reduced.h5', 'methyl_27_pancan_tcga_reduced.h5', 'methyl_450_pancan_tcga_reduced.h5', 'rna_pancan_tcga_reduced.h5', 'rna_isoforms_pancan_tcga_reduced.h5', 'mirna_pancan_tcga_reduced.h5', 'protein_pancan_tcga_reduced.h5']
+    # saving_files_names = ['exon_pancan_tcga.h5', 'cnv_pancan_tcga.h5', 'methyl_27_pancan_tcga.h5', 'methyl_450_pancan_tcga.h5', 
+    #                       'rna_pancan_tcga.h5', 'rna_isoforms_pancan_tcga.h5', 'mirna_pancan_tcga.h5', 'protein_pancan_tcga.h5']
+    # saving_files_names_reduced = ['exon_pancan_tcga_reduced', 'cnv_pancan_tcga_reduced', 'methyl_27_pancan_tcga_reduced', 
+    #                               'methyl_450_pancan_tcga_reduced', 'rna_pancan_tcga_reduced', 'rna_isoforms_pancan_tcga_reduced', 
+    #                               'mirna_pancan_tcga_reduced', 'protein_pancan_tcga_reduced']
+    saving_files_names = ['cnv_pancan_tcga.h5', 'methyl_450_pancan_tcga.h5', 'rna_pancan_tcga.h5','mirna_pancan_tcga.h5']
+    saving_files_names_reduced = ['cnv_pancan_tcga_reduced', 'methyl_450_pancan_tcga_reduced', 'rna_pancan_tcga_reduced', 'mirna_pancan_tcga_reduced']
     for idx, fichier in enumerate(fichiers_path):
         # read_chunk_file(fichier_path=fichier, saving_file_name=f'{graham_file_path_origin}/data_hdf5/{saving_files_names[idx]}', chunk_size=100000)
-        build_file_with_dimentionality_reduction(fichier_path=fichier, saving_file_name=f'{graham_file_path_origin}/data_hdf5/{saving_files_names_reduced[idx]}')
+        build_file_with_dimentionality_reduction(fichier_path=fichier, 
+                                                 saving_file_name=f'{graham_file_path_origin}/data_hdf5/{saving_files_names_reduced[idx]}_2000.h5',
+                                                 nb_features_selected=2000)
+        build_file_with_dimentionality_reduction(fichier_path=fichier, 
+                                                 saving_file_name=f'{graham_file_path_origin}/data_hdf5/{saving_files_names_reduced[idx]}_5000.h5',
+                                                 nb_features_selected=5000)
+        build_file_with_dimentionality_reduction(fichier_path=fichier, 
+                                                 saving_file_name=f'{graham_file_path_origin}/data_hdf5/{saving_files_names_reduced[idx]}_10000.h5',
+                                                 nb_features_selected=10000)
+
