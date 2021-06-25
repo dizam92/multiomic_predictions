f09f36e5637138054ab7108b33a6b6d9f0b7ee62
dizam92 <mazido@yahoo.fr>
2021-06-25 16:02:20 -0400

    dispatch for each view are ready

---
+++ dispatch_folder/optuna_model_dispatch_methyl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=16G
+#SBATCH --account=rrg-corbeilj-ac
+#SBATCH --mail-user=mazid-abiodoun.osseni.1@ulaval.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --time=5:00:00
+
+date
+SECONDS=0
+which python
+python3 /home/maoss2/PycharmProjects/multiomic_predictions/multiomic_modeling/models/models_optuna_version.py --d_input_enc 2000 --dataset_views_to_consider methyl --output_path /home/maoss2/scratch/optuna_test_output_methyl --data_size 2000 --db_name experiment_methyl_data_2000 --study_name experiment_methyl_data_2000
+diff=$SECONDS
+echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+date
+
+
