8779f314bdf91b28c1668b0895c78472a6216e66
dizam92 <mazido@yahoo.fr>
2021-08-23 11:06:24 -0400

    data loader updated

--- multiomic_modeling/data/data_loader.py
+++ multiomic_modeling/data/data_loader.py
@@ -3,7 +3,7 @@ import numpy as np
 import h5py
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler, Normalizer
-from sklearn.utils import class_weight
+from sklearn.utils import class_weight, compute_class_weight
 from copy import deepcopy
 from scipy.stats import median_absolute_deviation
 from torch.utils.data import Dataset, random_split, Subset, DataLoader, SubsetRandomSampler
@@ -11,53 +11,55 @@ from torch.nn.utils.rnn import pad_sequence
 from itertools import combinations
 
 files_path_on_graham = '/project/6000474/maoss2/tcga_pan_cancer_dataset/data_hdf5'
-# files_path_on_graham = '/Users/maoss2/PycharmProjects/multiomic_predictions/multiomic_modeling/data/tcga_pan_cancer_dataset'
 class FichierPath:
     cnv_file = f'{files_path_on_graham}/cnv_pancan_tcga_reduced_2000.h5'
     methyl450_file = f'{files_path_on_graham}/methyl_450_pancan_tcga_reduced_2000.h5'
     mirna_file = f'{files_path_on_graham}/mirna_pancan_tcga_reduced_2000.h5'
+    rna_file = f'{files_path_on_graham}/rna_pancan_tcga_reduced_2000.h5'
     rna_iso_file = f'{files_path_on_graham}/rna_isoforms_pancan_tcga_reduced_2000.h5'
-    exon_file = f'{files_path_on_graham}/exon_pancan_tcga_reduced.h5'
-    methyl27_file = f'{files_path_on_graham}/methyl_27_pancan_tcga_reduced.h5'
+    # exon_file = f'{files_path_on_graham}/exon_pancan_tcga_reduced.h5'
+    # methyl27_file = f'{files_path_on_graham}/methyl_27_pancan_tcga_reduced.h5'
     protein_file = f'{files_path_on_graham}/protein_pancan_tcga_reduced.h5'
-    rna_file = f'{files_path_on_graham}/rna_pancan_tcga_reduced.h5'
     survival_file = f'{files_path_on_graham}/Survival_SupplementalTable_S1_20171025_xena_sp'
     patients_without_view_file = f'{files_path_on_graham}/patients_without_view.txt'
-    patients_with_one_view_file = f'{files_path_on_graham}/patients_with_one_view.txt'
-    patients_with_two_or_more_views_file = f'{files_path_on_graham}/patients_with_two_or_more_views.txt'
-    patients_with_all_4_views_available_file = f'{files_path_on_graham}/patients_with_all_4_views_available.txt'
+    # patients_with_one_view_file = f'{files_path_on_graham}/patients_with_one_view.txt'
+    # patients_with_two_or_more_views_file = f'{files_path_on_graham}/patients_with_two_or_more_views.txt'
+    # patients_with_all_4_views_available_file = f'{files_path_on_graham}/patients_with_all_4_views_available.txt'
 
 class FichierPath5K:
     cnv_file = f'{files_path_on_graham}/cnv_pancan_tcga_reduced_5000.h5'
     methyl450_file = f'{files_path_on_graham}/methyl_450_pancan_tcga_reduced_5000.h5'
     mirna_file = f'{files_path_on_graham}/mirna_pancan_tcga_reduced_5000.h5'
-    rna_iso_file = f'{files_path_on_graham}/rna_isoforms_pancan_tcga_reduced_5000.h5'
+    rna_file = f'{files_path_on_graham}/rna_pancan_tcga_reduced_5000.h5'
+    # rna_iso_file = f'{files_path_on_graham}/rna_isoforms_pancan_tcga_reduced_5000.h5'
     protein_file = f'{files_path_on_graham}/protein_pancan_tcga_reduced.h5'
     exon_file = f'{files_path_on_graham}/exon_pancan_tcga_reduced.h5'
     survival_file = f'{files_path_on_graham}/Survival_SupplementalTable_S1_20171025_xena_sp'
     patients_without_view_file = f'{files_path_on_graham}/patients_without_view.txt'
-    patients_with_one_view_file = f'{files_path_on_graham}/patients_with_one_view.txt'
-    patients_with_two_or_more_views_file = f'{files_path_on_graham}/patients_with_two_or_more_views.txt'
-    patients_with_all_4_views_available_file = f'{files_path_on_graham}/patients_with_all_4_views_available.txt'
+    # patients_with_one_view_file = f'{files_path_on_graham}/patients_with_one_view.txt'
+    # patients_with_two_or_more_views_file = f'{files_path_on_graham}/patients_with_two_or_more_views.txt'
+    # patients_with_all_4_views_available_file = f'{files_path_on_graham}/patients_with_all_4_views_available.txt'
 
 class FichierPath10K:
     cnv_file = f'{files_path_on_graham}/cnv_pancan_tcga_reduced_10000.h5'
     methyl450_file = f'{files_path_on_graham}/methyl_450_pancan_tcga_reduced_10000.h5'
     mirna_file = f'{files_path_on_graham}/mirna_pancan_tcga_reduced_10000.h5'
-    rna_iso_file = f'{files_path_on_graham}/rna_isoforms_pancan_tcga_reduced_10000.h5'
+    rna_file = f'{files_path_on_graham}/rna_pancan_tcga_reduced_10000.h5'
+    # rna_iso_file = f'{files_path_on_graham}/rna_isoforms_pancan_tcga_reduced_10000.h5'
     protein_file = f'{files_path_on_graham}/protein_pancan_tcga_reduced.h5'
     exon_file = f'{files_path_on_graham}/exon_pancan_tcga_reduced.h5'
     survival_file = f'{files_path_on_graham}/Survival_SupplementalTable_S1_20171025_xena_sp'
     patients_without_view_file = f'{files_path_on_graham}/patients_without_view.txt'
-    patients_with_one_view_file = f'{files_path_on_graham}/patients_with_one_view.txt'
-    patients_with_two_or_more_views_file = f'{files_path_on_graham}/patients_with_two_or_more_views.txt'
-    patients_with_all_4_views_available_file = f'{files_path_on_graham}/patients_with_all_4_views_available.txt'
+    # patients_with_one_view_file = f'{files_path_on_graham}/patients_with_one_view.txt'
+    # patients_with_two_or_more_views_file = f'{files_path_on_graham}/patients_with_two_or_more_views.txt'
+    # patients_with_all_4_views_available_file = f'{files_path_on_graham}/patients_with_all_4_views_available.txt'
     
 def read_h5py(fichier, normalization=False) -> dict:
     d = h5py.File(fichier, 'r')
     data = d['dataset'][()]
     if normalization:
-        data = MinMaxScaler().fit_transform(data)
+        data = StandardScaler().fit_transform(data)
+        # data = MinMaxScaler().fit_transform(data)
     feature_names = np.asarray([el.decode("utf-8") for el in d['features_names'][()]])
     patient_names = np.asarray([el.decode("utf-8") for el in d['patients_names'][()]])
     patient_names = dict(zip(patient_names, np.arange(len(patient_names))))
@@ -116,9 +118,9 @@ def clean_patients_list_problem():
             f.write(f'{patient}\n')
 
 patients_without_view = read_file_txt(FichierPath.patients_without_view_file)
-patients_with_one_view_file = read_file_txt(FichierPath.patients_with_one_view_file)
-patients_with_two_or_more_views_file = read_file_txt(FichierPath.patients_with_two_or_more_views_file)
-patients_with_all_4_views_available_file = read_file_txt(FichierPath.patients_with_all_4_views_available_file)
+# patients_with_one_view_file = read_file_txt(FichierPath.patients_with_one_view_file)
+# patients_with_two_or_more_views_file = read_file_txt(FichierPath.patients_with_two_or_more_views_file)
+# patients_with_all_4_views_available_file = read_file_txt(FichierPath.patients_with_all_4_views_available_file)
 
 class BuildViews(object):
     def __init__(self, data_size, view_name):
@@ -128,19 +130,24 @@ class BuildViews(object):
             FichierPath.cnv_file = FichierPath5K.cnv_file
             FichierPath.methyl450_file = FichierPath5K.methyl450_file
             FichierPath.mirna_file = FichierPath5K.mirna_file
-            FichierPath.rna_iso_file = FichierPath5K.rna_iso_file
+            FichierPath.rna_file = FichierPath5K.rna_file
+            FichierPath.protein_file = FichierPath5K.protein_file
+            # FichierPath.rna_iso_file = FichierPath5K.rna_iso_file
         if data_size == 10000:
             FichierPath.cnv_file = FichierPath10K.cnv_file
             FichierPath.methyl450_file = FichierPath10K.methyl450_file
             FichierPath.mirna_file = FichierPath10K.mirna_file
-            FichierPath.rna_iso_file = FichierPath10K.rna_iso_file
+            FichierPath.rna_file = FichierPath10K.rna_file
+            FichierPath.protein_file = FichierPath10K.protein_file
+            # FichierPath.rna_iso_file = FichierPath10K.rna_iso_file
         if data_size not in [743, 2000, 5000, 10000]: raise ValueError(f'the data size {data_size} is not available in the dataset')
         if view_name == 'all':
             self.views = [
                 read_h5py(fichier=FichierPath.cnv_file, normalization=False), 
                 read_h5py(fichier=FichierPath.methyl450_file, normalization=False),
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True),
-                read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
+                read_h5py(fichier=FichierPath.mirna_file, normalization=False),
+                read_h5py(fichier=FichierPath.rna_file, normalization=False),
+                read_h5py(fichier=FichierPath.protein_file, normalization=False)
             ]
         elif view_name == 'cnv':
             self.views = [
@@ -152,65 +159,15 @@ class BuildViews(object):
             ]
         elif view_name == 'mirna':
             self.views = [
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True)
+                read_h5py(fichier=FichierPath.mirna_file, normalization=False)
             ]
         elif view_name == 'rna_iso':
             self.views = [
                 read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
             ]
-        elif view_name == 'cnv_methyl_rna':
+        elif view_name == 'rna':
             self.views = [
-                    read_h5py(fichier=FichierPath.cnv_file, normalization=False), 
-                    read_h5py(fichier=FichierPath.methyl450_file, normalization=False),
-                    read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
-                ]
-        elif view_name == 'cnv_methyl_mirna':
-             self.views = [
-                    read_h5py(fichier=FichierPath.cnv_file, normalization=False), 
-                    read_h5py(fichier=FichierPath.methyl450_file, normalization=False),
-                    read_h5py(fichier=FichierPath.mirna_file, normalization=True)
-                ]
-        elif view_name == 'methyl_mirna_rna':
-            self.views = [
-                read_h5py(fichier=FichierPath.methyl450_file, normalization=False),
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True),
-                read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
-            ]
-        elif view_name == 'cnv_mirna_rna':
-            self.views = [
-                read_h5py(fichier=FichierPath.cnv_file, normalization=False), 
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True),
-                read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
-            ]
-        elif view_name == 'cnv_mirna':
-            self.views = [
-                read_h5py(fichier=FichierPath.cnv_file, normalization=False),
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True)
-            ]
-        elif view_name == 'cnv_rna':
-            self.views = [
-                read_h5py(fichier=FichierPath.cnv_file, normalization=False),
-                read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
-            ]
-        elif view_name == 'cnv_methyl':
-            self.views = [
-                read_h5py(fichier=FichierPath.cnv_file, normalization=False), 
-                read_h5py(fichier=FichierPath.methyl450_file, normalization=False)
-            ]
-        elif view_name == 'mirna_rna':
-            self.views = [
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True),
-                read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
-            ]
-        elif view_name == 'methyl_mirna':
-            self.views = [
-                read_h5py(fichier=FichierPath.methyl450_file, normalization=False),
-                read_h5py(fichier=FichierPath.mirna_file, normalization=True)
-            ]
-        elif view_name == 'methyl_rna':
-            self.views = [
-                read_h5py(fichier=FichierPath.methyl450_file, normalization=False),
-                read_h5py(fichier=FichierPath.rna_iso_file, normalization=True)
+                read_h5py(fichier=FichierPath.rna_file, normalization=False)
             ]
         else:
             raise ValueError(f'The view {view_name} is not available in the dataset')
@@ -242,37 +199,18 @@ class MultiomicDataset(Dataset):
         self.survival_data = read_pandas_csv(fichier=FichierPath.survival_file)
         self.sample_to_labels = {self.survival_data['sample'].values[idx]: self.survival_data['cancer type abbreviation'].values[idx] 
                                  for idx, _ in enumerate(self.survival_data['sample'].values)}
-        for patient_name in patients_without_view:
-            self.sample_to_labels.pop(patient_name)
-        if views_to_consider == 'all':
-            for patient_name in list(self.sample_to_labels.keys()):
-                if patient_name not in patients_with_two_or_more_views_file:
-                    self.sample_to_labels.pop(patient_name)    
-        elif views_to_consider in ['cnv', 'methyl', 'mirna', 'rna_iso']:
-            patients_name_view = list(self.views[0]['patient_names'].keys())
-            for patient_name in list(self.sample_to_labels.keys()):
-                if patient_name not in patients_name_view:
-                    self.sample_to_labels.pop(patient_name) 
-        elif views_to_consider in ['cnv_methyl_rna', 'cnv_methyl_mirna', 'cnv_mirna_rna', 'methyl_mirna_rna', 
-                                   'cnv_mirna', 'cnv_rna', 'cnv_methyl', 'mirna_rna', 'methyl_mirna','methyl_rna']:
-            patients_name_views = []
-            for idx in range(len(self.views)):
-                patients_name_views.extend(list(self.views[idx]['patient_names'].keys()))
-            patients_name_views = list(np.unique(patients_name_views))
-            for patient_name in list(self.sample_to_labels.keys()):
-                if patient_name not in patients_name_views:
-                    self.sample_to_labels.pop(patient_name)
-        else:
-            raise ValueError(f'The view {views_to_consider} is not available in the dataset')
+        for patient_name in patients_without_view: # On elimine les patients sans vue (0 vue) donc anyway on peut rien faire avec
+            self.sample_to_labels.pop(patient_name)       
         self.all_patient_names = np.asarray(list(self.sample_to_labels.keys()))
         self.all_patient_labels = np.asarray(list(self.sample_to_labels.values()))
         self.label_encoder = LabelEncoder() # i will need this to inverse_tranform afterward i think for the analysis downstream
         self.all_patient_labels = self.label_encoder.fit_transform(self.all_patient_labels)
-        self.class_weights = class_weight.compute_class_weight('balanced',
-                                                 np.unique(self.all_patient_labels),
-                                                 self.all_patient_labels) #pylint deconne sinon pas d'erreurs
+        self.class_weights = compute_class_weight(class_weight='balanced',
+                                                  classes=np.unique(self.all_patient_labels),
+                                                  y=self.all_patient_labels) 
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx): 
+        idx = idx % self.__len__() # pour contrer le fait que la longueur du dataset pourrait etre supérieure à l'idx samplé
         patient_name = self.all_patient_names[idx]
         patient_label = self.all_patient_labels[idx]
         data = np.zeros((len(self.views), self.nb_features)) # nombre_views X nombre_features
@@ -283,16 +221,18 @@ class MultiomicDataset(Dataset):
                 except ValueError:
                     data[i][:view['data'][view['patient_names'].get(patient_name, 0)].shape[0]] = view['data'][view['patient_names'].get(patient_name, 0)]
         mask = np.array([(patient_name in view['patient_names']) for view in self.views])
-        original_mask = deepcopy(mask)
+        # original_mask = deepcopy(mask)
         nb_views = np.sum(mask)
-        if nb_views > 1:
-            n_views_to_drop = np.random.choice(nb_views-1)
-            if n_views_to_drop >= 1:
-                mask[np.random.choice(np.flatnonzero(mask), size=n_views_to_drop)] = 0
-        return (data.astype(float), mask, original_mask), patient_label
+        n_views_to_drop = np.random.choice(nb_views - 1) # maybe le contraindre? Pk j'avais penser ca? je voulais probablement le contraindre à 1 vue mais bon why? 
+        if n_views_to_drop >= 1:
+            mask[np.random.choice(np.flatnonzero(mask), size=n_views_to_drop)] = 0
+        original_data = deepcopy(data.astype(float))
+        data_augmentation = data.astype(float) * mask # on met à zéro la vue ou les vues qu'on a dit de drop
+        return (data_augmentation, mask), original_data, patient_label
+        # return (data.astype(float), mask, original_mask), patient_label
     
     def __len__(self):
-        return len(self.all_patient_names)
+        return len(self.all_patient_names) # estimer le nombre d'exemples que le dataset contient
                
 def multiomic_dataset_builder(dataset, test_size=0.2, valid_size=0.1):
     n = len(dataset)
