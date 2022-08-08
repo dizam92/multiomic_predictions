ad2bda9374c69e2216ee2773fc6646e9722f786e
dizam92 <mazido@yahoo.fr>
2022-08-08 17:21:21 -0400

    reformat results and figures analysis

--- multiomic_modeling/analyses_and_paper_figures_section/results_and_figures_analysis.py
+++ multiomic_modeling/analyses_and_paper_figures_section/results_and_figures_analysis.py
@@ -23,8 +23,8 @@ sns.set_theme()
 # home_path = '/home/maoss2/scratch'
 home_path = '/home/maoss2/PycharmProjects/multiomic_predictions/reports_dir'
 # results_order are always in this order 'acc', 'prec', 'rec', 'f1_score', 'mcc_score'
-best_config_file_path_normal_data_aug_2000 = '/home/maoss2/scratch/normal_output_2000_all_699/cdc7d218e2bdc3bc54394e8a002692e31fb7f1f7/config.json' 
-best_config_file_path_normal_normal_2000 = '/home/maoss2/scratch/data_aug_output_2000_all_699/31c120a9ff2e9020902ad5a62eb9ea8cb857573f/config.json'
+best_config_file_path_normal_normal_2000 = '/home/maoss2/scratch/normal_output_2000_all_699/cdc7d218e2bdc3bc54394e8a002692e31fb7f1f7/config.json' 
+best_config_file_path_normal_data_aug_2000 = '/home/maoss2/scratch/data_aug_output_2000_all_699/31c120a9ff2e9020902ad5a62eb9ea8cb857573f/config.json'
 
 class ResultsAnalysis:
     
@@ -879,7 +879,18 @@ def new_main():
     ResultsAnalysis().optuna_analysis_reports(directory='optuna_data_aug_3_main_omics_repo/', output_file='data_aug_3_main_omics_reports.md')
     ResultsAnalysis().optuna_analysis_reports(directory='optuna_data_aug_all_repo/', output_file='data_aug_all_reports.md')
     # Build Figures 
-    
+    # attention Weight Plot
+    # to run this: salloc --time=03:00:00 --nodes=1  --ntasks-per-node=16 --mem=64G --account=rrg-corbeilj-ac
+    main_attention_weights_plot(config_file=best_config_file_path_normal_normal_2000, 
+                                algo_type='normal', 
+                                exp_type='normal', 
+                                output_path=home_path, 
+                                data_size=2000)
+    main_attention_weights_plot(config_file=best_config_file_path_normal_data_aug_2000, 
+                            algo_type='normal', 
+                            exp_type='data_aug', 
+                            output_path=home_path, 
+                            data_size=2000)
 if __name__ ==  '__main__':
     # Baselines
     ResultsAnalysis().baselines_analysis_reports()
@@ -894,16 +905,6 @@ if __name__ ==  '__main__':
     fig_article.figure_1()
     fig_article.figure_2()
     fig_article.build_supplementary_figures()
-    # attention Weight Plot
-    # to run this: salloc --time=03:00:00 --nodes=1  --ntasks-per-node=16 --mem=64G --account=rrg-corbeilj-ac
-    main_attention_weights_plot(config_file=best_config_file_path_normal_normal_2000, 
-                                algo_type='normal', 
-                                exp_type='normal', 
-                                output_path=best_config_file_path_normal_normal_2000[:-12], 
-                                data_size=2000)
-    main_attention_weights_plot(config_file=best_config_file_path_normal_data_aug_2000, 
-                                algo_type='normal', 
-                                exp_type='data_aug', 
-                                output_path=best_config_file_path_normal_data_aug_2000[:-12], 
-                                data_size=2000)
+
+
     
